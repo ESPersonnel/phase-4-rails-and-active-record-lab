12cd2140256ee0b2a8e41303e6ab46d6425b6c63
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-10-07 11:56:52 +0300

    commit tree 231888e53a7107c1b6e76035c22d167839449c32

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  # Custom Routes
+  # HTTP Verb, URL, Controller#Action
+
+  get '/about', to: 'index#about'
+
+  # Resources: Generates all the routes for a given resource
+
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
